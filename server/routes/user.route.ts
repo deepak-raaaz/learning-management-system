d71b55689946c69d8683342353b2dd3bb3cb2541
deepak-raaaz <deepakjamui26@gmail.com>
2024-02-23 22:31:35 +0530

    update user info and course model design

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,15 +1,38 @@
-import express from 'express';
-import { loginUser, activateUser, registrationUser, logoutUser } from '../controllers/user.controller';
-import { authorizeRoles, isAuthenticated } from '../middleware/auth';
+import express from "express";
+import {
+  loginUser,
+  activateUser,
+  registrationUser,
+  logoutUser,
+  updateAccessToken,
+  getUserInfo,
+  socialAuth,
+  updateUserInfo,
+  updatePassword,
+  updateProfilePicture,
+} from "../controllers/user.controller";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
 
-userRouter.post('/registration', registrationUser);
+userRouter.post("/registration", registrationUser);
 
-userRouter.post('/activate-user', activateUser);
+userRouter.post("/activate-user", activateUser);
 
-userRouter.post('/login', loginUser);
+userRouter.post("/login", loginUser);
 
-userRouter.post('/logout',isAuthenticated, logoutUser);
+userRouter.get("/logout", isAuthenticated, logoutUser);
+
+userRouter.get("/refresh", updateAccessToken);
+
+userRouter.get("/me", isAuthenticated, getUserInfo);
+
+userRouter.post("/social-auth", socialAuth);
+
+userRouter.put("/update-user-info", isAuthenticated, updateUserInfo);
+
+userRouter.put("/update-user-password", isAuthenticated, updatePassword);
+
+userRouter.put("/update-user-avatar", isAuthenticated, updateProfilePicture);
 
 
 
